a84520f46663b614f7b0e86e058b9c2828acce01
Telminov Sergey <telminov@soft-way.biz>
2015-03-02 15:03:39 +0300

    setup file

---
+++ setup.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+from distutils.core import setup
+
+setup(
+    name='sw-python-utils',
+    version='0.0.1',
+    description='Soft Way company python utils.',
+    author='Telminov Sergey',
+    url='https://github.com/telminov/sw-python-utils',
+    packages=['swutils',],
+    license='The MIT License',
+    install_requires=[
+
+    ],
+)
