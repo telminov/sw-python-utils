c1476ce3c4723aec3efdc4ec417d637262000cbf
sergey@telminov.ru <sergey@telminov.ru>
2015-08-05 10:21:27 +0300

    one more test

---
+++ swutils/tests/test_network.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+import mock
+from unittest import TestCase
+import swutils.network
+
+class NetWorkTestCase(TestCase):
+
+    @mock.patch('netifaces.interfaces')
+    @mock.patch('netifaces.ifaddresses')
+    def test_get_ip4(self, ifaddresses_mock, interfaces_mock):
+        address = '10.0.1.4'
+        interfaces_mock.return_value = ['eth0']
+        ifaddresses_mock.return_value = {2: [{'broadcast': '10.0.1.255', 'netmask': '255.255.255.0', 'addr': address}]}
+
+        ip4 = swutils.network.get_ip4()
+        self.assertEquals(ip4, [address])
