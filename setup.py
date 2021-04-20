a47d754ea78f60108568c63465849fb063355794
Egor <egorka-raz@yandex.ru>
2021-04-20 14:11:18 +0300

    Обновил версию

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='sw-python-utils',
-    version='0.0.17',
+    version='0.0.18',
     description='Soft Way company python utils.',
     author='Telminov Sergey',
     url='https://github.com/telminov/sw-python-utils',
